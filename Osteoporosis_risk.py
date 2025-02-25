ca0e4ed585978f71c27bf94347083fda85b2f663
angelaseq <118992725+angelaseq@users.noreply.github.com>
2025-02-25 11:01:43 +0100

    Update Osteoporosis_risk.py

--- Osteoporosis_risk.py
+++ Osteoporosis_risk.py
@@ -14,7 +14,7 @@ np.random.seed(42)
 df = pd.read_csv("Osteoporosis.csv")
 
 # Dividi i dati in X (features) e y (target)
-x = df.drop('Osteoporosis', axis=0)
+x = df.drop('Osteoporosis', axis=1)
 y = df['Osteoporosis']
 
 # Suddividi i dati in training e test set
